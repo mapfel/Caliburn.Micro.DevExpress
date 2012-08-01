120a059b318623ab17707bf17d15f73f5ef1f98d
pjenei <none@none.com>
2012-08-01 22:32:40 +0000

    Initial version.

---
+++ Caliburn.Micro.DevExpress/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Caliburn.Micro.DevExpress")]
+[assembly: AssemblyDescription("Conventions and classes for using DevExpress WPF controls with Caliburn.Micro")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jenei Péter")]
+[assembly: AssemblyProduct("Caliburn.Micro.DevExpress")]
+[assembly: AssemblyCopyright("Copyright © Jenei Péter 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dc77e049-8d0b-453a-8cb0-9f1c21590d7d")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
