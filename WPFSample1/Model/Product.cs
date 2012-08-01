cf02974bdba81b1d8371fa17e6fdbc1e08704945
pjenei <none@none.com>
2012-08-01 22:32:40 +0000

    Initial version.

---
+++ WPFSample1/Model/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+
+namespace WPFSample1.Model
+{
+  public class Product
+  {
+    public int ID { get; set; }
+
+    public string Name { get; set; }
+
+    public static BindableCollection<Product> GetProductList()
+    {
+      BindableCollection<Product> list = new BindableCollection<Product>();
+      list.Add(new Product() { ID = 1, Name = "Coffee" });
+      list.Add(new Product() { ID = 2, Name = "Tea" });
+      list.Add(new Product() { ID = 3, Name = "Sugar" });
+      return list;
+    }
+  }
+}
