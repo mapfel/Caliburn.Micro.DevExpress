7acb15bb2963eac9df77274a447e14e4da623a4e
pjenei <none@none.com>
2012-08-01 22:32:40 +0000

    Initial version.

---
+++ WPFSample1/ShellView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPFSample1
+{
+  public partial class ShellView
+  {
+    protected virtual void bNew_ItemClick(object sender, EventArgs e)
+    {
+      ((IShell)DataContext).New();
+    }
+  }
+}
---
+++ WPFSample1/ShellViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WPFSample1
+{
+  using System.ComponentModel.Composition;
+  using Caliburn.Micro;
+  using Caliburn.Micro.DevExpress;
+
+  [Export(typeof(IShell))]
+  public class ShellViewModel : DevExpressConductorOneActive<IScreen>, IShell
+  {
+    private int count = 1;
+
+    public void New()
+    {
+      ActivateItem(new InnerDocumentViewModel() { DisplayName = "Inner document " + count++, CloseAction = this.CloseAction });
+    }
+  }
+}
